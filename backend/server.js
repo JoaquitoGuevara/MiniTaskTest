fe6d909bc617ef0801dd3d3a2053eb2109420373
JoaquitoGuevara <joaquitoguevara@gmail.com>
2024-12-20 01:10:48 -0300

    Updated backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+let tasks = [
+    { id: 1, task: 'Sample Task 1' },
+    { id: 2, task: 'Sample Task 2' },
+];
+
+app.get('/tasks', (req, res) => res.json(tasks));
+app.post('/tasks', (req, res) => {
+    const newTask = { id: Date.now(), task: req.body.task };
+    tasks.push(newTask);
+    res.json(newTask);
+});
+app.delete('/tasks/:id', (req, res) => {
+    tasks = tasks.filter(task => task.id !== parseInt(req.params.id));
+    res.status(204).send();
+});
+
+app.listen(6000, () => console.log('Backend running on http://localhost:6000'));
